8cc057a409a64bbf99cf72445df41ec55dab1126
Mike Mahony <mikem@animationmentor.com>
2014-04-29 13:54:01 -0700

    Tests refer to this package.

--- exampleclient_test.go
+++ exampleclient_test.go
@@ -1,15 +1,15 @@
 package turnpike_test
 
 import (
-	"github.com/jcelliott/turnpike"
+    "github.com/AnimationMentor/turnpike"
 )
 
 func ExampleClient_NewClient() {
-	c := turnpike.NewClient()
-	err := c.Connect("ws://127.0.0.1:8080/ws", "http://localhost/")
-	if err != nil {
-		panic("Error connecting:" + err.Error())
-	}
+    c := turnpike.NewClient()
+    err := c.Connect("ws://127.0.0.1:8080/ws", "http://localhost/")
+    if err != nil {
+        panic("Error connecting:" + err.Error())
+    }
 
-	c.Call("rpc:test")
+    c.Call("rpc:test")
 }
